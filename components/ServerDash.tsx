5a918ce454e145ff8e5057010957685c9f885b1f
tomo <tomo@crowdeo.com>
2025-02-05 22:47:51 +0700

    feat(ServerDash): Disable Server AsyncButton

--- components/ServerDash.tsx
+++ components/ServerDash.tsx
@@ -10,6 +10,7 @@ import { Bucket } from "@/db/bucket";
 import { CATEGORY_MAP } from "@/db/categories";
 import { useServerStats } from "@/hooks/useServerStats";
 import { ServerStatsPayload } from '@/db/database';
+import { useModifyServers } from '@/hooks/useModifyServers';
 import { useSyncPosts } from "@/hooks/useSyncPosts";
 import AsyncButton from "./AsyncButton";
 import ServerStats from "./ServerStats";
@@ -160,8 +161,9 @@ interface ServerDashProps {
   server: Server;
 }
 
-const ServerDash: React.FC<ServerDashProps> = ({ server }) => {
+const ServerfeatDash: React.FC<ServerDashProps> = ({ server }) => {
   const { data: stats, isPending: isStatsLoading, error: statsError, invalidateServerStats } = useServerStats(server.slug);
+  const { updateServer } = useModifyServers();
 
   const { mutateAsync: syncPosts } = useSyncPosts({
     server: server.slug,
@@ -171,6 +173,15 @@ const ServerDash: React.FC<ServerDashProps> = ({ server }) => {
   const handleSyncNewer = async () => {
     await syncPosts({ older: false, batch: 1 });
   };
+  const handleSyncNewer10x = async () => {
+    await syncPosts({ older: false, batch: 10 });
+  };
+  const disableServer = async (server: Server) => {
+      await updateServer({
+        id: server.id,
+        server: { ...server, enabled: false }, // Only update the `enabled` field
+      });
+    };
 
   if (isStatsLoading) {
     return <p>Loading stats for {server.name}...</p>;
@@ -188,13 +199,28 @@ const ServerDash: React.FC<ServerDashProps> = ({ server }) => {
       <h2 className="text-xl font-bold text-gray-800 mb-4">{server.name} /{" "}
         <Link href={`/${server.slug}/regular`} className="text-blue-500 hover:text-blue-600 underline">
           Regular ({stats?.categoryCounts[Bucket.regular]?.unseen || 0})
-        </Link></h2>
+        </Link>
       <AsyncButton
         callback={handleSyncNewer}
         loadingText="Syncing Newer..."
         defaultText="Collect Newer Posts"
         color="blue"
+        extraClasses="ml-2"
       />
+      <AsyncButton
+        callback={handleSyncNewer10x}
+        loadingText="Syncing Newer..."
+        defaultText="Collect Newer Posts 10x"
+        color="blue"
+        extraClasses="ml-2"
+      />
+      <AsyncButton
+        callback={() => disableServer(server)}
+        defaultText="Disable Server"
+        color="blue"
+        extraClasses="ml-2"
+      />
+      </h2>
       <div className="mt-4 p-4 border rounded shadow-sm bg-gray-50">
         {stats && (<ServerStats stats={stats} />)}
       </div>
