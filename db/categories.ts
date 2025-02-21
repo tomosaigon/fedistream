d3129884f5efe9cd9d00a01580584c0f21f9ffa4
tomo <tomo@crowdeo.com>
2025-02-21 21:00:19 +0700

    feat: reorder categories, saved the best for last

--- db/categories.ts
+++ db/categories.ts
@@ -3,17 +3,17 @@ import { Bucket } from './bucket';
 export const CATEGORY_MAP = [
   { slug: 'regular', bucket: Bucket.regular, label: 'Regular' },
   // { slug: 'subscribed', bucket: Bucket.subscribed, label: 'Subscribed' },
-  { slug: 'saved', bucket: Bucket.saved, label: 'Saved' },
   { slug: 'questions', bucket: Bucket.questions, label: 'Questions' },
   { slug: 'with-images', bucket: Bucket.withImages, label: 'Images' },
+  { slug: 'hashtags', bucket: Bucket.hashtags, label: 'Hashtags' },
+  { slug: 'with-links', bucket: Bucket.withLinks, label: 'Links' },
   { slug: 'videos', bucket: Bucket.videos, label: 'Videos' },
   // { slug: 'replies', bucket: Bucket.asReplies, label: 'Replies' },
   { slug: 'network-mentions', bucket: Bucket.networkMentions, label: 'Mentions' },
-  { slug: 'hashtags', bucket: Bucket.hashtags, label: 'Hashtags' },
-  { slug: 'with-links', bucket: Bucket.withLinks, label: 'Links' },
   { slug: 'from-bots', bucket: Bucket.fromBots, label: 'Bots' },
   { slug: 'non-english', bucket: Bucket.nonEnglish, label: 'Non-English' },
   { slug: 'reblogs', bucket: Bucket.reblogs, label: 'Reblogs' },
+  { slug: 'saved', bucket: Bucket.saved, label: 'Saved' },
 ] as const;
 
 export type Category = typeof CATEGORY_MAP[number];
